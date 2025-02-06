b73c9d06d79d8e914a4cee5706f99bc735d72e2c
Shubho7 <banerjeesubhodeep54@gmail.com>
2025-02-07 02:51:22 +0530

    re ranking docs to provide accurate responses

--- app.py
+++ app.py
@@ -106,9 +106,22 @@ def llm(context: str, prompt: str):
             },
         ],
     )
-    
+
     for chunk in response:
         if not chunk.get("done", False):
             yield chunk["message"]["content"]
         else:
             break
+
+# Re-rank docs
+def re_rank(documents: list[str]) -> tuple[str, list[int]]:
+    relevant_text = ""
+    relevant_text_ids = []
+
+    encoder_model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+    ranks = encoder_model.rank(prompt, documents, top_k=3)
+    for rank in ranks:
+        relevant_text += documents[rank["corpus_id"]]
+        relevant_text_ids.append(rank["corpus_id"])
+
+    return relevant_text, relevant_text_ids
